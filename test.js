c89410c553fe4c45a14ad96b4c84ca0d83d4bdc4
tunnckoCore <mameto_100@mail.bg>
2016-04-29 07:10:38 +0300

    fix tests

--- test.js
+++ test.js
@@ -8,29 +8,35 @@
 'use strict'
 
 var test = require('assertit')
-var lazyArrayify = require('./index')
+var lazy = require('./index')
+
+test('should expose `.arrayify` and `.isArray` methods', function (done) {
+  test.strictEqual(typeof lazy.arrayify, 'function')
+  test.strictEqual(typeof lazy.isArray, 'function')
+  done()
+})
 
 test('should return empty array on falsey values', function (done) {
-  test.deepEqual(lazyArrayify(null), [])
-  test.deepEqual(lazyArrayify(false), [])
-  test.deepEqual(lazyArrayify(undefined), [])
-  test.deepEqual(lazyArrayify(), [])
-  test.deepEqual(lazyArrayify(0), [])
-  test.deepEqual(lazyArrayify(''), [])
+  test.deepEqual(lazy.arrayify(null), [])
+  test.deepEqual(lazy.arrayify(false), [])
+  test.deepEqual(lazy.arrayify(undefined), [])
+  test.deepEqual(lazy.arrayify(), [])
+  test.deepEqual(lazy.arrayify(0), [])
+  test.deepEqual(lazy.arrayify(''), [])
   done()
 })
 
 test('should return array if array given', function (done) {
-  test.deepEqual(lazyArrayify(['']), [''])
-  test.deepEqual(lazyArrayify([1, 2, 3]), [1, 2, 3])
-  test.deepEqual(lazyArrayify([null, false]), [null, false])
-  test.deepEqual(lazyArrayify([0, undefined]), [0, undefined])
+  test.deepEqual(lazy.arrayify(['']), [''])
+  test.deepEqual(lazy.arrayify([1, 2, 3]), [1, 2, 3])
+  test.deepEqual(lazy.arrayify([null, false]), [null, false])
+  test.deepEqual(lazy.arrayify([0, undefined]), [0, undefined])
   done()
 })
 
 test('should arrayify truthy values', function (done) {
-  test.deepEqual(lazyArrayify(123), [123])
-  test.deepEqual(lazyArrayify(true), [true])
-  test.deepEqual(lazyArrayify('foo'), ['foo'])
+  test.deepEqual(lazy.arrayify(123), [123])
+  test.deepEqual(lazy.arrayify(true), [true])
+  test.deepEqual(lazy.arrayify('foo'), ['foo'])
   done()
 })
